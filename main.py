75720a00d21dd12cfdb4918aa95b512683eb7039
Daniil <daniil.fronc@mail.ru>
2020-12-20 19:23:04 +0300

    Обмазал все трай эксептами и дебагом, изменил мем

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import logging
-logging.basicConfig(filename="/home/ubuntu/sna_final/log.log", level=logging.DEBUG)
+import sys
 
+
+logging.basicConfig(filename="/home/ubuntu/sna_final/log.log", level=logging.DEBUG)
 
 try:
     import redis
     from flask import Flask, render_template
 except Exception as err:
+   logging.debug("Can't import libraries:")
    logging.debug(err)
+   sys.exit(1)
+
 
 try:
-    app = Flask(__name__)
     r = redis.Redis(host="172.31.42.182", port=6379)
     r.set('counter', 0)
 except Exception as err:
-   logging.debug(err)
+    ("Can not connect to DB:")
+    logging.debug(err)
+    sys.exit(1)
+
 
 @app.route('/')
 def index():
-    counter = int(r.get('counter')) + 1
+    cur = r.get('counter') if r.get('counter') else 0
+    counter = int(cur) + 1
     r.set('counter', counter)
     return render_template('index.html', counter=counter)
 
 if __name__ == "__main__":
     try:
+        app = Flask(__name__)
         app.run(host="0.0.0.0", port=8000)
     except Exception as err:
+        logging.debug("Error in the app:")
         logging.debug(err)
+        sys.exit(1)
