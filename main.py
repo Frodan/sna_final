fd1cd4cc72e8ffff0b65b65c80530231928c4846
Daniil <daniil.fronc@mail.ru>
2020-12-20 15:23:53 +0300

    test log

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import redis
 from flask import Flask, render_template
 import logging
 
+Logging.basicConfig(filename="/home/ubuntu/sna_final/log.log", level=(logging.INFO, logging.DEBUG, logging.ERROR))
+
 try:
     app = Flask(__name__)
     r = redis.Redis(host="172.31.42.182", port=6379)
